52e62c07486f122f31fd9c48e41ab2cd59c65858
fardado90 <55768801+fardado90@users.noreply.github.com>
2019-09-24 19:34:15 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+var Eris = require('eris');
+var bot = new Eris("NjI2MTcyMzM2ODY0NzU1NzEy.XYqRHg.Vus-wbFD440TLUhdaf24_h4k02g");
+
+bot.on("ready", () =>{
+	console.log("Estou Vivo!");
+	});
+
+bot.on("messageCreate", (msg)=>{
+
+	if(msg.content.indexOf("!radio")==0){
+		var num = Math.floor(Math.random() * 999);
+		bot.createMessage(msg.channel.id,"Radio do dia: "+num);
+	}
+
+});
+
+bot.connect();
