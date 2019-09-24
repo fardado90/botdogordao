95f415fc66cbb08bf32152aad4218a1717d73860
fardado90 <55768801+fardado90@users.noreply.github.com>
2019-09-24 20:43:01 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
+const cron = require('cron');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-	
-    if (message.content === '!radio') {
-    		var num = Math.floor(Math.random() * 999);
-	  	message.channel.send(' @everyone Radio do dia: '+num);
-	    	
-  	}
+let scheduledMessage = new cron.CronJob('15 43 20 * * *', () => {
+  // This runs every day at 10:30:00, you can do anything you want
+  let channel = yourGuild.channels.get('449300184422154282');
+  channel.send('You message');
 });
+
+// When you want to start it, use:
+scheduledMessage.start()
 
 
 
