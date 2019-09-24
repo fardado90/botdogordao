e14f973f1916a6b52bb4959ecabe4a28f9985543
fardado90 <55768801+fardado90@users.noreply.github.com>
2019-09-24 20:17:31 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,10 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '!radio') {
     		var num = Math.floor(Math.random() * 999);
+	  	var d = new Date();
+		var n = d.getHours();
 	   	message.channel.send(' @everyone Radio do dia: '+num);
+	    	message.channel.send(n);
   	}
 });
 
