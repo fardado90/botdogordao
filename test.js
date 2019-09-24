12cb6f62b6d7f03ef53808c595228843c3864f61
fardado90 <55768801+fardado90@users.noreply.github.com>
2019-09-24 20:40:40 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+	
+    if (message.content === '!radio') {
+    		var num = Math.floor(Math.random() * 999);
+	  	message.channel.send(' @everyone Radio do dia: '+num);
+	    	
+  	}
+});
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
 
